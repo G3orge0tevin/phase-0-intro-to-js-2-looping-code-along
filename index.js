ec314c4f447c5ce1875af41c73d251ad89ece176
Tevin <georgetevin0@gmail.com>
2024-07-07 21:36:01 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, giftType) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`);
+    }
+    return messages;
+}
+const names = ["Guadalupe", "Ollie", "Aki"];
+const messages = writeCards(names);
+console.log(messages);
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
